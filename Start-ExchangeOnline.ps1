86d50da96fd23eb9efb72ced521dc2471586d857
aafksab <36931057+aafksab@users.noreply.github.com>
2018-03-30 14:04:29 -0500

    Fixed issue with stale session

--- Start-ExchangeOnline.ps1
+++ Start-ExchangeOnline.ps1
@@ -1,4 +1,4 @@
-function Start-ExchangeOnline {
+function global:Start-ExchangeOnline {
    <#
        .Description
         connects to Exchange online services for the logged on user
@@ -13,25 +13,51 @@ function Start-ExchangeOnline {
 	param (
 		[string]
 		$upn = (Get-ADUser -Identity $env:USERNAME -Properties emailaddress | Select-Object -ExpandProperty emailaddress),
-		[validateset('s', 'Service', 'p', 'Protection', 'n', 'None')][string]
+		[validateset('S', 'Service', 'P', 'Protection', 'C', 'Continue')][string]
 		$session
 	)
 	
 	$session = read-host -Prompt 'Select your session; (S)ervice, (P)rotection, (N)one'
+	write-host "[-] Connecting to your Session..." -ForegroundColor green
 	
 	switch -Wildcard ($session) {
 		
 		S* {
-			Connect-EXOPSsession -userprincipalname $upn
+			$ErrorOccured = $false
+			try {
+				$ErrorActionPreference = 'Stop'
+				Connect-EXOPSsession -userprincipalname $upn
+			}
+			catch {
+				write-host '[!] Session has become stale...' -ForegroundColor Yellow
+				$ErrorOccured = $true
+			}
+			if ($ErrorOccured) {
+				Write-Host "[+] Generating Fresh Session now..." -ForegroundColor Green
+				Connect-EXOPSsession -userprincipalname $upn
+			}
 		}
 		
 		P* {
-			Connect-IPPSSession -userprincipalname $upn
+			$ErrorOccured = $false
+			try {
+				$ErrorActionPreference = 'Stop'
+				Connect-IPPSSession -userprincipalname $upn
+			}
+			catch {
+				write-host '[!] Session has become stale...' -ForegroundColor Yellow
+				$ErrorOccured = $true
+			}
+			if ($ErrorOccured) {
+				Write-Host "[+] Generating Fresh Session now..." -ForegroundColor Green
+				Connect-IPPSSession -userprincipalname $upn
+			}
 		}
-		N* {
+		C* {
 			Continue
 		}
 	}
+	
 }
 
 function Start-ExchangeOnlineNOAD {
@@ -45,35 +71,61 @@ function Start-ExchangeOnlineNOAD {
         start-ExchangeOnline -Session 'protection'
         start-ExchangeOnline -Session s
    #>
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory = $true)][ValidateScript({
-				If ($_ -match '(([\w\.\-]*)\@([\w\.\-]+))') {
-					$True
-				}
-				Else {
-					throw 'Please enter a valid UPN'
-				}
-			})]
-		[string]
-		$upn,
-		[validateset('s', 'Service', 'p', 'Protection', 'n', 'None')][string]
-		$session
-	)
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)][ValidateScript({
+          If ($_ -match '(([\w\.\-]*)\@([\w\.\-]+))') {
+            $True
+          }
+          Else {
+            throw 'Please enter a valid UPN'
+          }
+      })]
+    [string]
+    $upn,
+    [validateset('s', 'Service', 'p', 'Protection', 'n', 'None')][string]
+    $session
+  )
+  
+  $session = read-host -Prompt 'Select your session; (S)ervice, (P)rotection, (N)one'
+  write-host "[-] Connecting to your Session..." -ForegroundColor green
 	
-	$session = read-host -Prompt 'Select your session; (S)ervice, (P)rotection, (N)one'
+  switch -Wildcard ($session) {
+		
+    S* {
+      $ErrorOccured = $false
+      try {
+        $ErrorActionPreference = 'Stop'
+        Connect-EXOPSsession -userprincipalname $upn
+      }
+      catch {
+        write-host '[!] Session has become stale...' -ForegroundColor Yellow
+        $ErrorOccured = $true
+      }
+      if ($ErrorOccured) {
+        Write-Host "[+] Generating Fresh Session now..." -ForegroundColor Green
+        Connect-EXOPSsession -userprincipalname $upn
+      }
+    }
+		
+    P* {
+      $ErrorOccured = $false
+      try {
+        $ErrorActionPreference = 'Stop'
+        Connect-IPPSSession -userprincipalname $upn
+      }
+      catch {
+        write-host '[!] Session has become stale...' -ForegroundColor Yellow
+        $ErrorOccured = $true
+      }
+      if ($ErrorOccured) {
+        Write-Host "[+] Generating Fresh Session now..." -ForegroundColor Green
+        Connect-IPPSSession -userprincipalname $upn
+      }
+    }
+    C* {
+      Continue
+    }
+  }
 	
-	switch -Wildcard ($session) {
-		
-		S* {
-			Connect-EXOPSsession -userprincipalname $upn
-		}
-		
-		P* {
-			Connect-IPPSSession -userprincipalname $upn
-		}
-		N* {
-			Continue
-		}
-	}
 }
